80665b3cbe9780497f03a69dd7f443c56e173629
Dillon Wyatt <dostaglou@gmail.com>
2022-11-14 01:43:33 +0900

    initial gql query working

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://localhost:3000/api/graphql',
+    uri: 'https://69f4-240f-65-33a0-1-d57c-d02f-ec5a-9f2d.jp.ngrok.io/graphql',
     cache: new InMemoryCache(),
 });
 
--- src/MainScreen.js
+++ src/MainScreen.js
@@ -4,17 +4,26 @@ import { gql } from '@apollo/client'
 
 const link = "https://69f4-240f-65-33a0-1-d57c-d02f-ec5a-9f2d.jp.ngrok.io"
 const headers = { Accept: 'application/json', 'Content-Type': 'application/json'}
-const details = { method: 'GET', headers: headers }
+const details = { method: 'POST', headers: headers }
+
+async function getData() {
+  const response = await client.query({
+    query: gql`query { testField }`,
+    fetchPolicy: "network-only"
+  })
+  if (!response.ok) {
+    console.log("err", response)
+    return("error")
+  }
+
+  const data = response.json();
+  console.log("data", data) 
+
+  return(data)
+}
 
 export default function MainScreen() {
-  fetch(link, details)
-    .then((response) => response.json())
-    .then((json) => {
-      console.log("json", json)
-    })
-    .catch ((error) => {
-      console.log("error", error)
-    })
+  getData()
 
   return (
     <View>
