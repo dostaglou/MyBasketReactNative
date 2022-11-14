d7727f6593b0dff71c18e3f3cf5e0758d46e894d
Dillon Wyatt <dostaglou@gmail.com>
2022-11-14 21:23:55 +0900

    wip2

--- apollo-client.js
+++ apollo-client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const baseLink = "https://9fa4-240f-65-33a0-1-d57c-d02f-ec5a-9f2d.jp.ngrok.io"
 const makeLink = () => { `${baseLink}/graphql` }
-const headers = { "Auth-Token": "56c8753b-e8e2-4b32-b728-660089391bc4" }
+const headers = { Accept: 'application/json', 'Content-Type': 'application/json', "Auth-Token": "56c8753b-e8e2-4b32-b728-660089391bc4" }
 
 const client = new ApolloClient({
     uri: "https://9fa4-240f-65-33a0-1-d57c-d02f-ec5a-9f2d.jp.ngrok.io/graphql",
