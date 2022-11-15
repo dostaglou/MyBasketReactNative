0a84e1a60635300de8e83dfa2c0d45f0d478b9cd
Dillon Wyatt <dostaglou@gmail.com>
2022-11-15 20:11:13 +0900

    working

--- apollo-client.js
+++ apollo-client.js
@@ -1,11 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const baseLink = "https://9fa4-240f-65-33a0-1-d57c-d02f-ec5a-9f2d.jp.ngrok.io"
-const makeLink = () => { `${baseLink}/graphql` }
+const baseLink = "https://575a-240f-65-33a0-1-d57c-d02f-ec5a-9f2d.jp.ngrok.io"
+const makeLink =  `${baseLink}/graphql`
 const headers = { Accept: 'application/json', 'Content-Type': 'application/json', "Auth-Token": "56c8753b-e8e2-4b32-b728-660089391bc4" }
 
 const client = new ApolloClient({
-    uri: "https://9fa4-240f-65-33a0-1-d57c-d02f-ec5a-9f2d.jp.ngrok.io/graphql",
+    uri: makeLink,
     cache: new InMemoryCache(),
     headers: headers
 });
--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native'
-import client from '../apollo-client'
 import { gql, useQuery } from '@apollo/client'
 
 const QUERY_STRING = gql`query { privateData }`
 
 export default function MainScreen() {
   const { loading, error, data } = useQuery(QUERY_STRING)
+
   if (loading) { return(<Text>"loading"</Text>)}
   if (error) { return(<Text>"error"</Text>) }
 
