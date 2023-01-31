164ca6371ad9fbbf63a872c096ee96688439bd0f
Dillon Wyatt <dostaglou@gmail.com>
2023-02-01 00:44:29 +0900

    using env vars

--- utils/apollo.js
+++ utils/apollo.js
@@ -1,8 +1,7 @@
 
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const externalLink = "d895-240f-65-33a0-1-8869-cefa-2be2-c3d2"
-const url = `https://${externalLink}.jp.ngrok.io/graphql`
+const url = `https://${process.env.NGROK}.jp.ngrok.io/graphql`
 
 const makeApolloClient = (token) => {
   const client = new ApolloClient({
