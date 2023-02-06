7a006bc760496e047462c4d1eac74e38eb5f3662
Dillon Wyatt <dostaglou@gmail.com>
2023-02-06 20:07:28 +0900

    WIP: main page listing with query and mutation but currently invalid hook

--- utils/apollo.js
+++ utils/apollo.js
@@ -1,9 +1,10 @@
 
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const url = `https://${process.env.NGROK}.jp.ngrok.io/graphql`
+const baseUrl = "https://URL-POINTER.jp.ngrok.io/graphql"
 
 const makeApolloClient = (token) => {
+  let url = baseUrl.replace("URL-POINTER", process.env.NGROK)
   const client = new ApolloClient({
     uri: url,
     cache: new InMemoryCache(),
